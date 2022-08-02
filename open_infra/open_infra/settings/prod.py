4b70dd16eb7caa80d00d7c3064b7486703f6d9b9
TomNewChao <353712216@qq.com>
2022-08-02 16:02:57 +0800

    0802_modify secret to pwd

--- open_infra/open_infra/settings/prod.py
+++ open_infra/open_infra/settings/prod.py
@@ -26,7 +26,7 @@ sys.path.insert(0, os.path.join(BASE_DIR, "apps"))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("secret")
+SECRET_KEY = os.getenv("pwd")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
