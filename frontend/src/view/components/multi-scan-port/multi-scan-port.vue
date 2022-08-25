c137905b3cc806f6daa0e17b4f261ee66f35e179
TomNewChao <353712216@qq.com>
2022-08-25 17:48:08 +0800

    finsh test

---
+++ frontend/src/view/components/multi-scan-port/multi-scan-port.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <Card>
+      <tables ref="tables" editable searchable search-place="top" v-model="tableData" :columns="columns"
+              @on-delete="handleDelete"/>
+      <Button style="margin: 10px 2px 2px;" type="primary" @click="exportExcel">导出</Button>
+
+    </Card>
+  </div>
+</template>
+
+<script>
+import Tables from '_c/tables'
+import { downloadScanPortExcelApi, scanPortApi } from '@/api/tools'
+import { blobDownload } from '@/libs/download.js'
+import { getStrDate } from '@/libs/tools.js'
+
+export default {
+  name: 'tables_page',
+  components: {
+    Tables
+  },
+  data () {
+    return {
+      columns: [
+        { title: 'Account', key: 'account', sortable: true },
+        { title: 'Zone', key: 'zone', sortable: true },
+        {
+          title: 'Handle',
+          key: 'handle',
+          options: ['delete'],
+          button: [
+            (h, params, vm) => {
+              return h('Poptip', {
+                props: {
+                  confirm: true,
+                  title: '你确定要删除吗?'
+                },
+                on: {
+                  'on-ok': () => {
+                    vm.$emit('on-delete', params)
+                    vm.$emit('input', params.tableData.filter((item, index) => index !== params.row.initRowIndex))
+                  }
+                }
+              })
+            }
+          ]
+        }
+      ],
+      tableData: []
+    }
+  },
+  mounted () {
+    this.scanPort()
+  },
+  methods: {
+    handleDelete (params) {
+    },
+    exportExcel () {
+      let selectName = []
+      for (let i = 0; i < this.tableData.length; i++) {
+        selectName.push(this.tableData[i].account)
+      }
+      if (selectName.length === 0) {
+        this.$Message.info('请至少选择一个Account。')
+      } else {
+        downloadScanPortExcelApi(selectName).then(res => {
+          if (res.headers['content-type'] === 'application/octet-stream') {
+            const strDate = getStrDate()
+            const fileName = 'IP高危端口扫描统计表_' + strDate + '.xlsx'
+            blobDownload(res.data, fileName)
+          }
+        })
+      }
+    },
+    scanPort () {
+      scanPortApi().then(res => {
+        this.tableData = res.data.data
+      })
+    }
+  }
+}
+</script>
---
+++ frontend/src/view/components/sla/sla.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <Card>
+      <div class="sla-search-con search-con-top">
+        <DatePicker type="date" placeholder="Select date" confirm v-model="slaDate" style="width: 200px"/>
+        <Select v-model="slaSearchKey" class="search-col">
+          <Option v-for="item in slaFilterColumns" :value="item.key" :key="item.key">{{ item.title }}</Option>
+        </Select>
+        <Input clearable placeholder="输入关键字搜索" class="search-input" v-model="slaSearchValue"/>
+        <Button @click="handleSearch" class="search-btn" type="primary">
+          <Icon type="search"/>&nbsp;&nbsp;搜索
+        </Button>
+        <Button type="primary" @click="slaHandleSubmit" class="sla-export">导出所有数据</Button>
+      </div>
+      <tables ref="tables" search-place="top" v-model="slaTableData" :columns="slaColumns"/>
+      <Page :total="slaPageTotal" :current="slaPageNum" :page-size="slaPageSize" show-sizer show-total
+            @on-change="slaHandlerPage"
+            @on-page-size-change="slaHandlerPageSize"/>
+    </Card>
+  </div>
+</template>
+
+<script>
+import Tables from '_c/tables'
+import './index.less'
+import { slaListApi, exportSlaData } from '@/api/tools'
+import { getStrDate } from '@/libs/tools'
+import { blobDownload } from '@/libs/download'
+
+export default {
+  name: 'tables_page',
+  components: {
+    Tables
+  },
+  data () {
+    return {
+      slaDate: '',
+      slaSearchKey: '',
+      slaSearchValue: '',
+      slaPageTotal: 10,
+      slaPageNum: 1,
+      slaPageSize: 10,
+      order_by: 'sla_year_remain',
+      order_type: '0',
+      slaFilterColumns: [
+        { title: '服务名', key: 'service_name' }
+      ],
+      slaColumns: [
+        { title: '服务名', key: 'service_name' },
+        { title: '服务介绍', key: 'introduce' },
+        { title: '访问地址', key: 'sla_url' },
+        { title: '社区', key: 'sla_zone' },
+        { title: '月度异常累计时间(min)', key: 'month_exp_min' },
+        { title: '年度异常累计时间(min)', key: 'year_exp_min' },
+        { title: '月度SLA', key: 'month_sla' },
+        { title: '年度SLA', key: 'year_sla' },
+        { title: '年度剩余SLA配额', key: 'sla_year_remain' }
+      ],
+      slaTableData: []
+    }
+  },
+  mounted () {
+    this.setSlaDate()
+    this.querySlaList()
+  },
+  methods: {
+    setSlaDate () {
+      let time = new Date()
+      let month = time.getMonth() + 1
+      let day = time.getDate()
+      let nowTime = time.getFullYear() + '-' + (month < 10 ? '0' + month : month) + '-' + (day < 10 ? '0' + day : day)
+      this.slaDate = nowTime + 'T16:00:00.000Z'
+    },
+    handleSearch () {
+      this.querySlaList()
+    },
+    slaHandlerPage (value) {
+      this.slaPageNum = value
+      this.querySlaList()
+    },
+    slaHandlerPageSize (value) {
+      this.slaPageSize = value
+      this.querySlaList()
+    },
+    querySlaList () {
+      slaListApi(this.slaPageNum, this.slaPageSize, this.order_by, this.order_type, this.slaSearchKey, this.slaSearchValue, this.slaDate).then(res => {
+        console.log(this.slaDate)
+        if (res.data.err_code !== 0) {
+          this.$Message.info(res.data.description)
+        } else {
+          this.slaTableData = res.data.data.data
+          this.slaPageTotal = res.data.data.total
+          this.slaPageNum = res.data.data.page
+          this.slaPageSize = res.data.data.size
+        }
+      })
+    },
+    slaHandleSubmit () {
+      exportSlaData().then(res => {
+        if (res.headers['content-type'] === 'application/octet-stream') {
+          let strDate = getStrDate()
+          const fileName = 'Sla数据统计表_' + strDate + '.xlsx'
+          blobDownload(res.data, fileName)
+        }
+      })
+    }
+  }
+}
+</script>
+<style>
+.ivu-page {
+  margin-top: 30px;
+  text-align: center;
+}
+
+.search-col {
+  margin-left: 3px;
+}
+.sla-export {
+  margin-left: 5px;
+}
+</style>
