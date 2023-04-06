b926a32e68afb4e65149a8d7cce77cd612f24fc7
TomNewChao <353712216@qq.com>
2023-04-06 17:37:23 +0800

    the django admin add the search fields

--- open_infra/open_infra/apps/app_resources/admin.py
+++ open_infra/open_infra/apps/app_resources/admin.py
@@ -6,14 +6,17 @@ from open_infra.utils.admin import BaseAdmin
 
 class ServiceInfoAdmin(BaseAdmin):
     list_display = ["service_name", "namespace", "cluster", "region"]
+    search_fields = ["service_name", "namespace", "cluster", "region"]
 
 
 class ServiceSlaAdmin(BaseAdmin):
     list_display = [field.name for field in ServiceSla._meta.get_fields()]
+    search_fields = ["url", "service_alias", "service_zone", "service__service_name"]
 
 
 class ServiceImageAdmin(BaseAdmin):
     list_display = [field.name for field in ServiceImage._meta.get_fields()]
+    search_fields = ["image", "developer", "email", "service__service_name"]
 
 
 admin.site.register(ServiceInfo, ServiceInfoAdmin)
